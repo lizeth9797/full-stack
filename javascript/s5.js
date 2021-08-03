ce2c81b66a4143f586f51e737286e091555ff1b3
lizeth9797 <karina.ortiz@fintecheando.mx>
2021-08-02 22:12:15 -0500

    sesion 6, herencia

---
+++ javascript/s5.js
@@ -0,0 +1,7 @@
+/*
+----------------
+RETO 01             
+----------------
+Crear una función power que recibe dos argumentos base y exponent. La función debe retornar el resultado de elevar base a la potencia exponent.
+Evitar usar el operador de potenciación ** o la función Math.pow()
+*/
