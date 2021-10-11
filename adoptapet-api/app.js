3ae00a32c83a71e20f8ccbddd0bf78ba44c340e9
lizeth9797 <karina.ortiz@fintecheando.mx>
2021-10-11 07:16:12 -0500

    DB info

--- adoptapet-api/app.js
+++ adoptapet-api/app.js
@@ -8,10 +8,11 @@ app.use(bodyParser.json());
 
 //Configuración de la base de datos
 const mongoose = require('mongoose');
-mongoose.connect(
+/* mongoose.connect(
   process.env.MONGODB_URI, // obtiene la url de conexión desde las variables de entorno
   { useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true }
-);
+); */
+mongoose.connect('mongodb+srv://admin:Passw0rd@cluster0.xxifa.mongodb.net/Adoptpet?retryWrites=true&w=majority')
 mongoose.set("debug",true)//activación de la función debug
 require('./models/Mascota')
 require('./models/Usuario')
@@ -19,7 +20,11 @@ require('./models/Solicitud')
 
 app.use('/v1',require('./routes'));
 
-//const PORT = 4001;
-app.listen(process.env.PORT, () => {
+const PORT = 4001;
+/* app.listen(process.env.PORT, () => {
   console.log(`Server is listening on port ${process.env.PORT}`);
+}); */
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
 });
