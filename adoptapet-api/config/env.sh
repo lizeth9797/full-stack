cbc3d085bd99a6f38a4ed46ca5276fd151898da7
lizeth9797 <karina.ortiz@fintecheando.mx>
2021-10-11 20:10:23 -0500

    react

--- adoptapet-api/config/env.sh
+++
@@ -1,4 +0,0 @@
-export NODE_ENV='development'
-export PORT=4001
-export SECRET='secret'
-export MONGO_URI='mongodb+srv://admin:Passw0rd@cluster0.xxifa.mongodb.net/Adoptpet?retryWrites=true&w=majority'
