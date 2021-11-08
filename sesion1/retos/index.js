cec3e54d5a8fb95f0306291a5db1b8654533d27b
lizeth9797 <karina.ortiz@fintecheando.mx>
2021-11-07 23:45:00 -0600

    reto 02 completado

--- sesion1/retos/index.js
+++ sesion1/retos/index.js
@@ -1,4 +1,3 @@
-
 //RETO 01
 /* function suma(a,b){
 console.log(a+b)
@@ -6,18 +5,47 @@ console.log(a+b)
 suma(4,10) */
 
 //RETO 02
-/* const myEmitter = new MyEmitter();
-myEmitter.emit('error', new Error('whoops!')); */
-/* const saludo=require('./saludo')
-console.log(saludo('juan'))
+
+// Parte 01
+/* const http = require('http');
+const hostname='127.0.0.1'
+const port=3000
+const server=http.createServer((req,res)=>{
+     res.statusCode=200;
+     res.setHeader('Content-Type', 'JSON');
+     res.end('Hello')
+});
+
+server.listen(port,hostname,()=>{
+     console.log(`Server running at http://${hostname}:${port}/`)
+})
 */
 
+// Parte 02
+/* var colors = require('colors');
+console.log('hello'.green); // outputs green text
+console.log('i like cake and pies'.underline.red) // outputs red underlined text
+console.log('inverse the color'.inverse); // inverses the color
+console.log('OMG Rainbows!'.rainbow); // rainbow
+console.log('Run the trap'.trap); // Drops the bass
+  */
+
+// Parte 03
+/* const saludo=require('./saludo')
+console.log(saludo('juan')) */
+
+
+
+
+
 //RETO 03
-const random=require('./random')
+/* const random=require('./random')
 
 let desordenado=[]
 for (i=0; i<100; i++){
      desordenado.push(random(-1000,1000,10))
 }
 let ordenado=desordenado.sort((a,b)=>a-b) //manera correcta para sortear, sino, sólo ordenará como String, ASCII
-console.log(ordenado)
+console.log(ordenado) */
+
+
